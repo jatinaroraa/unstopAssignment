859d8e11d106b19a9c493173b307427811e4a3d8
jatin arora <jatinaroraaaa3@gmail.com>
2023-04-02 21:38:47 +0530

    push to host

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./config/development");
 const bodyParser = require("body-parser");
+const path = require("path");
 const connection = mongoose
   .connect(config.mongo.url)
   .then(() => {
@@ -17,6 +18,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 require("./routes")(app);
+
+app.use(express.static(__dirname + "/unstopProject"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/unstopProject/index.html"));
+});
+route.get("/", (req, res) => {
+  console.log("hit home");
+});
 app.listen(3000, () => {
   console.log("lisening");
 });
